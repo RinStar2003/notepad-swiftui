60921992c8c929d8255233eb5cc37915f7bfe5e2
RinStar2003 <mituha133@gmail.com>
2022-08-27 16:07:24 +0300

    Initial Commit

---
+++ Shared/notepad_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  notepad_swiftuiApp.swift
+//  Shared
+//
+//  Created by мас on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct notepad_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
