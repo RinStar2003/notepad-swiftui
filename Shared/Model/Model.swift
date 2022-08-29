7f59b0a8132a59118e9d5ea27a2223b2d7bc1e3a
RinStar2003 <mituha133@gmail.com>
2022-08-29 13:16:17 +0300

    - Model

--- Shared/Model/Model.swift
+++ Shared/Model/Model.swift
@@ -5,8 +5,26 @@
 //  Created by мас on 29.08.2022.
 //
 
-import Foundation
+import SwiftUI
 
 struct Note: Identifiable {
-    
+    var id = UUID()
+        .uuidString
+    var note: String
+    var date: Date
+    var cardColor: Color
 }
+
+func getDate(offset: Int) -> Date {
+    let calendar = Calendar.current
+    
+    let date = calendar.date(byAdding: .day, value: offset, to: Date())
+    
+    return date ?? Date()
+}
+
+var notes: [Note] = [
+
+
+    
+]
