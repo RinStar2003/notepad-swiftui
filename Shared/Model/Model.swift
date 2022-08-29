b04c74f9a90a5ecefe9f7967afe48950823b1beb
RinStar2003 <mituha133@gmail.com>
2022-08-29 18:12:29 +0300

    - Design finished - App can work both MacOS and iOS

--- Shared/Model/Model.swift
+++ Shared/Model/Model.swift
@@ -25,6 +25,11 @@ func getDate(offset: Int) -> Date {
 
 var notes: [Note] = [
 
+    Note(note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", date: getDate(offset: 1), cardColor: Color("blue")),
+    Note(note: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ", date: getDate(offset: -10), cardColor: Color("green")),
+    Note(note: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", date: getDate(offset: -15), cardColor: Color("orange")),
+    Note(note: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", date: getDate(offset: 10), cardColor: Color("pink")),
+    Note(note: "wqfanbsfbajfvajkjhbf`jhfajhbasjhbwr", date: getDate(offset: -3), cardColor: Color("purple")),
+    Note(note: "fasfqgx", date: getDate(offset: -8), cardColor: Color("yellow"))
 
-    
 ]
--- Shared/View/MainView.swift
+++ Shared/View/MainView.swift
@@ -28,15 +28,20 @@ struct MainView: View {
             // Main
             MainContent()
         }
+        #if os(macOS)
         .ignoresSafeArea()
+        #endif
         .frame(width: isMacOS() ? getRectangle().width / 1.7 : nil, height: isMacOS() ? getRectangle().height - 180 : nil, alignment: .leading)
         .background(Color("background").ignoresSafeArea())
+        #if os(iOS)
+        .overlay(SideBar())
+        #endif
         .preferredColorScheme(.light)
     }
     
     @ViewBuilder
     func MainContent() -> some View {
-        VStack(spacing: 15) {
+        VStack(spacing: 8) {
             
             // Search Bar
             HStack(spacing: 10) {
@@ -49,20 +54,33 @@ struct MainView: View {
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.bottom, isMacOS() ? 0 : 10)
-        
+            .overlay(
+                Rectangle()
+                    .fill(Color.gray.opacity(0.15))
+                    .frame(height: 1)
+                    .padding(.horizontal, -25)
+                    .offset(y: 8),
+                alignment: .bottom
+            )
             ScrollView(.vertical, showsIndicators: false) {
                 
                 VStack(spacing: 15) {
                     Text("Notes")
                         .font(isMacOS() ? .system(size: 33, weight: .bold) : .largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
                     // Columns
                     
                     let columns = Array(repeating: GridItem(.flexible(),spacing: isMacOS() ? 25 : 15), count: isMacOS() ? 3 : 1)
                     
                     LazyVGrid(columns: columns,spacing: 25) {
+                        ForEach(notes) { notes in
                         
+                            // Card
+                            CardView(note: notes)
+                        }
                     }
+                    .padding(.top, 30)
                 }
                 .padding(.top, isMacOS() ? 45 : 30)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -75,16 +93,52 @@ struct MainView: View {
     }
     
     @ViewBuilder
+    func CardView(note: Note) -> some View {
+        VStack {
+            Text(note.note)
+                .font(isMacOS() ? .title3 : .body)
+                .multilineTextAlignment(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            HStack {
+                Text(note.date, style: .date)
+                    .foregroundColor(.black)
+                    .opacity(0.9)
+                
+                Spacer(minLength: 0)
+                
+                // Edit button
+                Button {
+                    
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                        .font(.system(size: 15, weight: .bold))
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                }
+            }
+            .padding(.top, 55)
+        }
+        .padding()
+        .background(note.cardColor)
+        .cornerRadius(15)
+    }
+    
+    @ViewBuilder
     func SideBar() -> some View {
         VStack {
+            if isMacOS() {
             Text("Notepad +")
                 .font(.title)
                 .fontWeight(.semibold)
-            
+            }
             // Button
+            if isMacOS() {
             AddButton()
                 .zIndex(1)
-            
+            }
             VStack(spacing: 15) {
                 
                 let colors = [Color("blue"),Color("green"),Color("orange"),Color("pink"),Color("purple"),Color("yellow")]
@@ -92,18 +146,34 @@ struct MainView: View {
                 ForEach(colors, id: \.self) { c in
                     Circle()
                         .fill(c)
-                        .frame(width: 25, height: 25)
+                        .frame(width: isMacOS() ? 20 : 25, height: isMacOS() ? 20 : 25)
                 }
             }
             .padding(.top, 25)
             .frame(height: isColorShowed ? nil : 0)
             .opacity(isColorShowed ? 1 : 0)
             .zIndex(0)
+            
+            if !isMacOS() {
+                AddButton()
+                    .zIndex(1)
+            }
         }
+        #if os(macOS)
         .frame(maxHeight: .infinity, alignment: .top)
         .padding(.vertical)
         .padding(.horizontal, 22)
         .padding(.top, 35)
+        #else
+        .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .bottomTrailing)
+        .padding()
+        // Bluring
+        .background(
+            BlurView(style: .systemUltraThinMaterialDark).ignoresSafeArea()
+                .opacity(isColorShowed ? 1 : 0)
+                .ignoresSafeArea()
+        )
+        #endif
     }
     
     @ViewBuilder
